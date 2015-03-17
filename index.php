44f96fbe305d78b12de7a08b153d348bb168d2f3
Rubén Lacasa Mas <ruben@ensenalia.com>
2015-03-17 12:06:00 +0100

    Modificaciones
    Modificaciones

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (isset($_GET["error"])) {
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<html lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="robots" content="noindex, nofollow">
